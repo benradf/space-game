d086000264602b5f4a481775f103f97f12bb86b2
Ben Radford <benradf@users.noreply.github.com>
2009-02-25 17:41:59 +0000

    More updates to network library.
    First pass at packet serialisation code.

--- common/src/net/net.cpp
+++ common/src/net/net.cpp
@@ -2,6 +2,8 @@
 #include <enet/enet.h>
 #include <core.hpp>
 #include <assert.h>
+#include <boost/iostreams/stream.hpp>
+#include <boost/iostreams/device/array.hpp>
 
 
 net::Peer::Peer(void* data)
@@ -21,12 +23,56 @@ void net::Peer::sendPing(uint64_t time)
 
 void net::Peer::sendKeyExchange(uint64_t key)
 {
-
+    packetBegin();
+    packetWrite(key);
+    packetEnd();
 }
 
 void net::Peer::sendPlayerLogin(const char* username, char (&password)[16])
 {
+    packetBegin();
+    packetWrite(username);
+    packetWrite(password);
+    packetEnd();
+}
 
+void net::Peer::packetBegin()
+{
+    _packet = enet_packet_create(0, 1024, ENET_PACKET_FLAG_RELIABLE);
+    _packetWriter.open(static_cast<char*>(_packet->data), packet->dataLength);
+}
+
+void net::Peer::packetEnd()
+{
+    size_t size = _packetWriter.tellp();
+    assert(size <= _packet.dataLength);
+    enet_packet_resize(_packet, size);
+    _packet.Writer.close();
+}
+
+template<typename T>
+void net::Peer::packetWrite(T value)
+{
+    assert(_packetWriter.is_open());
+
+    _packetWriter << value;
+}
+
+template<typename T, int N>
+void net::Peer::packetWrite(T (&array)[N])
+{
+    assert(_packetWriter.is_open());
+
+    for (int i = 0; i < N; i++) 
+        writer << array[i];
+}
+
+void net::Peer::packetWrite(const char* str)
+{
+    assert(_packetWriter.is_open());
+
+    uint16_t len = strlen(str);
+    _packetWriter << len << str;
 }
 
 void net::Peer::handlePing(uint64_t time)
@@ -49,11 +95,12 @@ net::Interface::Interface()
 {
     // need to do enet_initialize() outside net::Interface since we may have multiple InterfaceS
 
-    _address.host = ENET_HOST_ANY;
-    _address.port = 12345;
+    ENetAddress address;
+    address.host = ENET_HOST_ANY;
+    address.port = 12345;
 
-    if ((_host = enet_host_create(&_address, 1024, 0, 0)) == 0)
-        throw NetworkException("failed to initialise enet");
+    if ((_host = enet_host_create(&address, 1024, 0, 0)) == 0)
+        throw NetworkException("enet_host_create failed");
 
 
 }
@@ -61,6 +108,26 @@ net::Interface::Interface()
 net::Interface::~Interface()
 {
     enet_host_destroy(_host);
+}
+
+void* net::Interface::connect(const char* host, uint16_t port)
+{
+    ENetAddress address;
+    enet_address_set_host(&address, host);
+    address.port = port;
+
+    ENetPeer* peer = 0;
+    if ((peer = enet_host_connect(_host, &address, 1)) == 0) 
+        throw NetworkException("enet_host_connect failed");
+
+    _connecting.insert(peer);
+
+    return peer;
+}
+
+bool net::Interface::connectionInProgress(void* data) const
+{
+    return (_connecting.find(data) != _connecting.end());
 }
 
 void net::Interface::doTasks()
@@ -81,9 +148,11 @@ void net::Interface::doTasks()
 
 void net::Interface::eventConnect(ENetEvent& event)
 {
-    PeerID id = event.peer->address.host;
+    _connecting.erase(event.peer);
+
+    PeerID id = makePeerID(event.peer->address);
     std::auto_ptr<Peer> peer = handleConnect(&event);
-    _peers.insert(std::make_pair(id, peer.get()));
+    _connected.insert(std::make_pair(id, peer.get()));
     peer.release();
 }
 
@@ -94,13 +163,15 @@ void net::Interface::eventReceive(ENetEvent& event)
 
 void net::Interface::eventDisconnect(ENetEvent& event)
 {
-    PeerID id = event.peer->address.host;
-    PeerMap::iterator iter = _peers.find(id);
-    assert(iter != _peers.end());
+    _connecting.erase(event.peer);
+
+    PeerID id = makePeerID(event.peer->address);
+    PeerMap::iterator iter = _connected.find(id);
+    if (iter == _connected.end()) 
+        return;
 
     std::auto_ptr<Peer> peer(iter->second);
-    _peers.erase(iter);
-
+    _connected.erase(iter);
     handleDisconnect(peer);
 }
 
--- common/src/net/net.hpp
+++ common/src/net/net.hpp
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <enet/enet.h>
 #include <tr1/unordered_map>
+#include <tr1/unordered_set>
 #include <memory>
 
 namespace net {
@@ -13,22 +14,46 @@ class Peer {
         Peer(void* data);
         virtual ~Peer();
 
+        void disconnect();
+
         void sendPing(uint64_t time);
         void sendKeyExchange(uint64_t key);
         void sendPlayerLogin(const char* username, char (&password)[16]);
 
     private:
+        void packetBegin();
+        void packetEnd();
+        
+        template<typename T>
+        void packetWrite(T value);
+        template<typename T, int N>
+        void packetWrite(T (&array)[N]);
+        void packetWrite(const char* str);
+
+        ENetPacket* _packet;
+        PacketWriter _packetWriter;
+
         virtual void handlePing(uint64_t time);
         virtual void handleKeyExchange(uint64_t key);
         virtual void handlePlayerLogin(const char* username, char (&password)[16]);
+
+        typedef boost::iostreams::stream<boost::iostreams::array_sink> PacketWriter;
 };
 
-typedef uint32_t PeerID;
+typedef uint64_t PeerID;
+inline PeerID makePeerID(ENetAddress& address)
+{
+    return (static_cast<PeerID>(address.port) << 32) | address.host;
+}
 
 class Interface {
     public:
         Interface();
         virtual ~Interface();
+
+        void* connect(const char* host, uint16_t port);
+
+        bool connectionInProgress(void* data) const;
 
         void doTasks();
         
@@ -40,11 +65,12 @@ class Interface {
         virtual std::auto_ptr<Peer> handleConnect(void* data);
         virtual void handleDisconnect(std::auto_ptr<Peer> peer);
 
-        ENetAddress _address;
-        ENetHost* _host;
-
         typedef std::tr1::unordered_map<PeerID, Peer*> PeerMap;
-        PeerMap _peers;
+        typedef std::tr1::unordered_set<void*> PeerSet;
+
+        ENetHost* _host;
+        PeerMap _connected;
+        PeerSet _connecting;
 };
 
 }  // namespace net
