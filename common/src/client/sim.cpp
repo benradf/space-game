d5ce0247a26d12c8f3ceafd7067b86a6b58bbfb5
Ben Radford <benradf@users.noreply.github.com>
2009-03-25 20:22:17 +0000

    Started work on physics simulation.

---
+++ common/src/client/sim.cpp
@@ -0,0 +1,14 @@
+/// \file sim.cpp
+/// \brief Physics simulation.
+/// \author Ben Radford 
+/// \date 25th March 2009
+///
+/// Copyright (c) 2009 Ben Radford. All rights reserved.
+///
+
+
+#include "sim.hpp"
+
+
+
+
---
+++ common/src/client/sim.hpp
@@ -0,0 +1,28 @@
+/// \file sim.cpp
+/// \brief Physics simulation.
+/// \author Ben Radford 
+/// \date 25th March 2009
+///
+/// Copyright (c) 2009 Ben Radford. All rights reserved.
+///
+
+
+#ifndef SIM_HPP
+#define SIM_HPP
+
+class RigidBody {
+    public:
+
+    private:
+};
+
+class Simulation {
+    public:
+
+    private:
+
+};
+
+
+#endif  // SIM_HPP
+
