490d47a71fd67879fe57c743f73974546e16064b
Ben Radford <benradf@users.noreply.github.com>
2009-05-06 16:55:31 +0100

    Created script to copy DLL dependencies.

---
+++ scripts/cp-dll-depends.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function cp-dlls() {
+    pe_file=$1
+    target_dir=$2
+    search_dirs=$3
+
+    strings $pe_file | grep "\.dll" | while read dll; do
+        if [ ! -e $target_dir/$dll ]; then
+            for search_dir in $search_dirs; do
+                if [ -f $search_dir/$dll ]; then
+                    cp -v $search_dir/$dll $target_dir
+                    cp-dlls "$search_dir/$dll" "$target_dir" "$search_dirs"
+                fi
+            done
+        fi
+    done
+}
+
+rm $2/*.dll
+cp-dlls $@
+
